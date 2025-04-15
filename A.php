c90c5a6f0e287344fa60861bbb97c217d15deb68
Vinícius Oliveira <vinicius.oli7dev@gmail.com>
2025-04-14 21:36:47 -0300

    Adicionando Classes A e B

---
+++ A.php
@@ -0,0 +1,31 @@
+<?php
+
+class A {
+    int A1;
+    float A2;
+
+    public function setA1() {
+        return A1;
+    }
+
+    public function getA1() {
+        return A1;
+    }
+
+    public function setA2() {
+        return A2;
+    }
+
+    public function getA2() {
+        return A2;
+    }
+
+    public function MA1(){
+        echo "Metodo - MA1";
+    }
+
+    public function MA2(){
+        echo "Metodo - MA2";
+    }
+}
+
---
+++ B.php
@@ -0,0 +1,28 @@
+<?php
+
+class B {
+    int B1;
+    float B2;
+
+    public function setB1() {
+        return B1;
+    }       
+    public function getB1() {
+        return B1;
+    }
+    public function setB2() {
+        return B2;
+    }
+    public function getB2() {
+        return B2;
+    }
+
+    public function MB1(){
+        echo "Metodo - MB1";
+    }
+
+    public function MB2(){
+        echo "Metodo - MB2";
+    }
+}
+
