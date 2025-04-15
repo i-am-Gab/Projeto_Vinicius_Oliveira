c0d4377f5be8d0b09b9bf30c000f588159a82c3a
Vinícius Oliveira <vinicius.oli7dev@gmail.com>
2025-04-14 22:21:22 -0300

    Criação da Classe C

---
+++ C.php
@@ -0,0 +1,28 @@
+<?php
+
+class C {
+    string C1;
+    int C2;
+
+    public function setC1() {
+        return C1;
+    }       
+    public function getC1() {
+        return C1;
+    }
+    public function setC2() {
+        return C2;
+    }
+    public function getC2() {
+        return C2;
+    }
+
+    public function MC1(){
+        echo "Metodo - MC1";
+    }
+
+    public function MC2(){
+        echo "Metodo - MC2";
+    }
+}
+
